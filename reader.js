6d43312ca85c2bf37767601cf1a6b6dcb4cb7d79
Nat <nat.wrw@gmail.com>
2020-07-19 17:17:36 +0700

    add reader

--- reader.js
+++ reader.js
@@ -1,6 +1,21 @@
 let db = require('./db.json')
+const puppeteer = require('puppeteer')
 // console.log(db)
 
-for (const dbElement of db) {
-    console.log(dbElement.link)
+let scrape = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    for (const dbElement of db) {
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1280, height: 800 });
+        await page.goto(dbElement.link, {waitUntil: 'networkidle2'});
+
+        await page.close()
+        // await page.waitForNavigation({
+        //     waitUntil: 'networkidle0',
+        // });
+        // await page.waitForSelector('#q')
+        // await page.type('#q', 'monitor');
+        // await page.keyboard.type(String.fromCharCode(13));
+    }
 }
+scrape()
